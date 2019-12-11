c3df62b8a3af6f82fcaca487e3fdf71b3ff32ed1
Jafar Miles <jafar@accurx.com>
2019-12-11 20:19:54 +0000

    roughly working...

--- main.py
+++ main.py
@@ -3,15 +3,11 @@ from marvel import getMarvelCharacter
 
 app = Flask("MyApp")
 
+
 @app.route("/")
-def base():
-    name = "Iron Man"
-    character = getMarvelCharacter(name)
-    # Now we have the weather, we want to pass through to the html file
-    # We are doing this in a json object to make it easy to pass through
-    # multiple values in one 'package', so first we put together the object
-    result = {"imageUrl": character, "name":name}
-    return render_template("character.html", result=result)
+def index():
+    return render_template("index.html")
+
 
 # We need a route to handle the form data coming from the html pack
 # Need to make sure the methods match and the / route matches the action in the form
@@ -20,18 +16,27 @@ def lookup():
     # Pull the form data from the request object
     form_data = request.form
     print(form_data)
-    # That form data is a dictonary object
-    # So can access parts of it in the usual way
-    city = form_data["search"]
-    print(city)
-    # Call all our API function with our form value
-    name = "Hulk"
+
+    isHawkeye = form_data['arrows'] == 'yes'
+    isIronman = form_data['iron'] == 'yes'
+    isCaptainAmerica = form_data['america'] == 'yes'
+    isTheHulk = form_data['temper'] == 'yes'
+
+    if isHawkeye:
+        name = "Hawkeye"
+    elif isIronman:
+        name = "Iron man"
+    elif isCaptainAmerica:
+        name = "Captain America"
+    elif isTheHulk:
+        name = "Hulk"
+    else:
+        name = "squirrel girl"
+
     character = getMarvelCharacter(name)
-    x = getWeather(city)
-    # Now we have the weather, we want to pass through to the html file
-    # We are doing this in a json object to make it easy to pass through
-    # multiple values in one 'package', so first we put together the object
+
     result = {"imageUrl": character, "name":name}
     return render_template("character.html", result=result)
+
 
 app.run(debug=True)
